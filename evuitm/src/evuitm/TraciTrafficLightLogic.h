d0c22ee223d6df1cbcf2592df05e5be5012273a4
syr4f <me.syraf@outlook.com>
2022-07-23 14:20:12 +0000

    add logic modules for TLS

--- evuitm/src/evuitm/TraciTrafficLightLogic.h
+++ evuitm/src/evuitm/TraciTrafficLightLogic.h
@@ -13,10 +13,71 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#ifndef EVUITM_TRACITRAFFICLIGHTLOGIC_H_
-#define EVUITM_TRACITRAFFICLIGHTLOGIC_H_
+#pragma once
 
-class TraciTrafficLightLogic {
+#include "evuitm/evuitm.h"
+#include "veins/modules/world/traci/trafficLight/logics/TraCITrafficLightAbstractLogic.h"
+#include "veins/modules/world/traci/trafficLight/TraCITrafficLightInterface.h"
+#include "veins/modules/mobility/traci/TraCICommandInterface.h"
+#include "veins/modules/utility/TimerManager.h"
+#include "veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h"
+#include "evuitm/TrafficLightBeacon_m.h"
+
+namespace evuitm {
+
+class EVUITM_API TraciTrafficLightLogic : public veins::TraCITrafficLightAbstractLogic {
+
+public:
+    TraciTrafficLightLogic();
+    ~TraciTrafficLightLogic();
+
+protected:
+    void initialize() override;
+    void handleApplMsg(cMessage* msg) override;
+    void handleTlIfMsg(veins::TraCITrafficLightMessage* tlMsg) override;
+    void handlePossibleSwitch() override;
+    void handleSelfMsg(cMessage* msg) override;
+
+private:
+
+    enum class PreemptionRequestSource {
+        EMERGENCY,
+        TL_LOGIC
+    };
+
+    /* interfacing */
+    veins::TraCITrafficLightInterface* tlInterfaceAccess;
+    veins::TraCICommandInterface* traci;
+    veins::TimerManager timerManager {this};  // for scheduling start of preemption
+
+    /* fields needed to control program switching*/
+    std::string tl_id;
+    veins::TimerManager::TimerHandle start_pref_msg;
+    int old_phase_num;              // to save index phase from which we switched to preemption
+    std::string old_program_id;     // used to save program id from which we switched to preemption
+    bool ev_should_stop_asking;
+    bool tl_switched_back;          // preemption is over, EV is away, do not switch to old_phase again
+
+
+    void startPreemption(const std::vector<int>&);
+    void switchProgramBack();
+    void scheduleStartPreemptionMsg(PreemptionRequestSource, simtime_t, const std::vector<int>&);
+
+    /* traffic light program helpers */
+    bool isPreemptionAllowed(std::string program_id, int phase_id);
+    simtime_t getActualPhaseTime();
+    std::vector<int> getIndicesOfGreensFromPhase(std::string);
+    std::string getDesiredPhase(const std::vector<int>&, int);
+
+    veins::TraCITrafficLightProgram::Logic createPreemptionProgram(std::string, std::string, std::vector<int>);
+
+
 };
 
-#endif /* EVUITM_TRACITRAFFICLIGHTLOGIC_H_ */
+} // namespace evuitm
+
+
+
+
+
+
