99bc69555b840f6a9154f7932f836f33699f9c28
syr4f <me.syraf@outlook.com>
2022-07-23 11:11:54 +0000

    Traffic Light Pre-emption Algorithm

--- evuitm/src/evuitm/TraciAlgorithm.h
+++ evuitm/src/evuitm/TraciAlgorithm.h
@@ -16,7 +16,22 @@
 #ifndef EVUITM_TRACIALGORITHM_H_
 #define EVUITM_TRACIALGORITHM_H_
 
-class TraciAlgorithm {
-};
+namespace evuitm{
 
+const double desired_speed = 70/3.6;    // desired speed of emergency vehicle [m/s]
+const double t_x = 1.22;                // average reaction time of a driver [s]
+const double t_a = 5.82;                // average time to accelerate to saturation speed [s]
+const double L_hn = 18.7;               // saturation gap between vehicles [m]
+const double q_ns = 0.5208;             // vehicle discharge speed [veh/s]
+const double t_sw = 11.0;               // minimal time necessary to get back to the current phase if the phase is switched at this very moment [s]
+const double t_min = 10.0;              // minimal time of green phase (green on main road cannot be shorter than t_min) [s]
+const double t_conservative = 0.0;      // time added to preemption start, to make it more conservative [s]
+
+double linApproxQueueDischarge(int num_vehicles, double t);
+double calc_TA(double distance, double speed = desired_speed);
+double calc_TL(int num_vehicles);
+double calc_TX(int num_vehicles, double TL, double speed = desired_speed);
+double expDelay(double time);  // expected delay created by additional cycle time
+
+}
 #endif /* EVUITM_TRACIALGORITHM_H_ */
